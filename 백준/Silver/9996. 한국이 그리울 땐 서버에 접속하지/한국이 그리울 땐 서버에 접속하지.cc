ffbadd883458c1d2832505b594c0e79d15ec217d
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2023-07-08 22:59:35 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str, pattern, out[100];
+    int n;
+    
+    cin >> n;
+    cin >> pattern;
+    
+    int pos = pattern.find('*');
+    string first = pattern.substr(0, pos); // 별표까지 자르고
+    string last = pattern.substr(pos+1, pattern.length()); // 별표부터 끝까지 자르고
+    
+    for(int i=0;i<n;i++) {
+        cin >> str; // 문자열 입력받고
+        if(str.length() < first.length() + last.length()) {
+            cout << "NE" << "\n";
+            continue;
+        }
+        
+        string strFirst = str.substr(0,first.length());
+        string strLast = str.substr(str.length()-last.length(), str.length());
+        
+        if(first==strFirst && last==strLast) {
+            cout << "DA" << "\n";
+        }
+        else {
+            cout << "NE" << "\n";
+        }
+    }
+    
+//    for(int i=0;i<n;i++) {
+//        cout << out[i] << endl;
+//    }
+    
+    return 0;
+}
