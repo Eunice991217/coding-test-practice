946f5c3abb79e10818ae615badfe42a8427f76a2
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-03-21 22:19:10 +0900

    [Silver III] Title: N과 M (8), Time: 220 ms, Memory: 21308 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static List<Integer> list;
+    static List<Integer> arr;
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+        arr = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++) {
+            int x = Integer.parseInt(st.nextToken());
+            arr.add(x);
+        }
+
+        Collections.sort(arr);
+
+        go(0, 0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    public static void go(int x, int start) throws IOException {
+        if(list.size()==m) {
+            for(int i=0;i<m;i++) {
+                bw.write(list.get(i)+" ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for(int i=start;i<n;i++) {
+            list.add(arr.get(i));
+            go(arr.get(i), i);
+            list.remove(list.size()-1);
+        }
+
+    }
+
+
+}
+
