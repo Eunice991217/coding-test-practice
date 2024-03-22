362fbf12675e3b3969ed24ea33330f40679f3c70
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-03-22 23:32:07 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 300 ms, Memory: 19288 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,104 +1,73 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
 import java.util.StringTokenizer;
 
-class Main {
-    static int n; // 집의 크기
-    static int ret = 0;
+public class Main {
+
+    static int n, ret=0;
     static int map[][];
-    static int dy[] = {1, 1, 0};
     static int dx[] = {0, 1, 1};
+    static int dy[] = {1, 1, 0};
 
-    public static void main(String args[]) throws Exception {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-        map = new int[n + 1][n + 1];
 
-        for (int i = 1; i <= n; i++) {
+        map = new int[n+1][n+1];
+
+        for(int i=1;i<=n;i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
+            for(int j=1;j<=n;j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        if (map[n][n]==1) {
-    	   System.out.println(0);
-    	   return;
-    	}
 
-        bfs(1, 2, 0);
+        go(1, 2, 0); // 0 : 가로
+
         System.out.println(ret);
 
     }
 
-    public static void bfs(int x, int y, int d) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(x, y, d));
+    public static void go(int x, int y, int dir) {
+        if(x<1 || x>=n+1 || y<1 || y>=n+1) return;
+        if(map[x][y]==1) return;
+        if(dir==1 && checkCross(x, y)) return;
 
-        while (!q.isEmpty()) {
-            Pair cur = q.poll();
-            int nx = cur.x;
-            int ny = cur.y;
-            int nd = cur.d;
+        // 0 : 가로, 1 : 대각선, 2 : 세로
+        if(x==n && y==n) {
+            ret++;
+            return;
+        }
 
-            if (nx == n && ny == n) {
-                ret++;
-                continue;
-            }
+        // 가로, 세로
+        if(dir==0 || dir==2) {
+            go(x+dx[dir], y + dy[dir], dir); // 가로 or 세로
+            go(x+dx[1], y+dy[1], 1); // 대각선
+        }
 
-            for (int i = 0; i < 3; i++) {
-                // 0 : 가로, 1 : 대각선, 2 : 세로
-                if (nd == 0 && i == 2) { // 가로
-                    continue;
-                }
-                if (nd == 2 && i == 0) { // 세로
-                    continue;
-                }
-                int ax = dx[i] + nx;
-                int ay = dy[i] + ny;
-                if (ax >= 1 && ax <= n && ay >= 1 && ay <= n && map[ax][ay] != 1) {
-
-                    // 다음 꺼가 대각선 일 때
-                    // 가로, 세로, 대각선 중에 1이 있으면
-                    // break;
-//                    if (map[ax][ay] == 1) {
-//                        continue;
-//                    }
-                    if (i == 1) {
-                        if (map[nx][ny + 1] == 1) {
-                            continue; // 가로
-                        }
-                        if (map[nx + 1][ny] == 1) {
-                            continue; // 세로
-                        }
-                    }
-                    q.add(new Pair(ax, ay, i));
+        // 대각선
+        else {
+            for(int i=0;i<3;i++) {
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+                if(nx>=1 && nx<=n && ny>=1 && ny<=n) {
+                    go(nx, ny, i);
                 }
             }
         }
 
     }
 
-
-}
-
-class Pair {
-    int x;
-    int y;
-    int d;
-
-    Pair(int x, int y, int d) {
-        this.x = x;
-        this.y = y;
-        this.d = d;
+    public static boolean checkCross(int x, int y) {
+        if(x-1>=1 && x-1<=n && map[x-1][y]==1) return true;
+        if(y-1>=1 && y-1<=n && map[x][y-1]==1) return true;
+        return false;
     }
+
+
+
 }
-
-
 
