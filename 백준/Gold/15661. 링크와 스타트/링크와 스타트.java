167bb1e9826c43c3fb3006effa27f6b26aaa700a
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-03-28 20:50:55 +0900

    [Gold V] Title: 링크와 스타트, Time: 540 ms, Memory: 24080 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,69 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 
-public class Main {
+class Main {
 
-    static int n;
-    static int arr[][];
-    static boolean visited[];
-    static int tmp[];
-    static String out[] = new String[1000004];
-    static int ret = Integer.MAX_VALUE;
+    static int n, ans=Integer.MAX_VALUE;
+    static int map[][];
+    static List<Integer> sList;
+    static List<Integer> lList;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][n];
-        visited = new boolean[n];
+        map = new int[n][n];
+        sList = new ArrayList<>();
+        lList = new ArrayList<>();
 
-        int cnt=1;
         for(int i=0;i<n;i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<n;j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=1;i<n;i++) {
-            combi(0, 0, i);
-        }
+        go(0, 0, 0);
 
-        System.out.print(ret);
+        System.out.println(ans);
 
     }
 
-    public static void combi(int start, int depth, int r) {
-        if(depth==r) {
-            int sTeam=0, lTeam=0;
-            for(int i=0;i<n-1;i++) {
-                for(int j=i+1;j<n;j++) {
-                    if(visited[i] == true && visited[j]==true) {
-                        sTeam += arr[i][j] + arr[j][i];
-                    }
-                    else if(visited[i]==false && visited[j]==false) {
-                        lTeam += arr[i][j] + arr[j][i];
-                    }
-                }
-
+    public static void go(int idx, int sSum, int lSum) {
+        if(idx>=n) {
+            if(sList.size()>=1 && lList.size()>=1 && sList.size()+lList.size()==n) {
+                ans = Math.min(ans, Math.abs(sSum-lSum));
             }
-            ret = Math.min(ret, Math.abs((sTeam-lTeam)));
             return;
         }
 
-        for(int i=start;i<n;i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                combi(i+1, depth+1, r);
-                visited[i] = false;
-            }
+        int ts=0;
+        for(int i=0;i<sList.size();i++) {
+            ts+=map[sList.get(i)][idx];
+            ts+=map[idx][sList.get(i)];
         }
+        sList.add(idx);
+        go(idx+1, sSum+ts, lSum);
+        sList.remove(sList.size()-1);
+//        go(idx+1, sSum+ts, lSum);
 
+        int ls=0;
+        for(int i=0;i<lList.size();i++) {
+            ls+=map[lList.get(i)][idx];
+            ls+=map[idx][lList.get(i)];
+        }
+        lList.add(idx);
+        go(idx+1, sSum, lSum+ls);
+        lList.remove(lList.size()-1);
+//        go(idx+1, sSum, lSum+ls);
     }
 
 
