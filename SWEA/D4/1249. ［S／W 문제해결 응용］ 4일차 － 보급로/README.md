15a747b1a075651001e716ebce826474e6bcc65f
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-05-18 02:01:22 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 256 ms, Memory: 48,200 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 응용] 4일차 - 보급로 - 1249 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15QRX6APsCFAYD) 
+
+### 성능 요약
+
+메모리: 48,200 KB, 시간: 256 ms, 코드길이: 1,995 Bytes
+
+### 제출 일자
+
+2024-05-18 02:01
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    static int n, ans;
+    static int map[][], v[][], sum[][];
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc=1;tc<=T;tc++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+
+            map = new int[n][n];
+            v = new int[n][n];
+            sum = new int[n][n];
+            for(int i=0;i<n;i++) {
+                String line = br.readLine();
+                for(int j=0;j<n;j++) {
+                    map[i][j] = line.charAt(j)-'0';
+                }
+            }
+
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
+                    sum[i][j]=Integer.MAX_VALUE;
+                }
+            }
+
+            sum[0][0]=0;
+            solve();
+            System.out.println("#" + tc + " " + sum[n-1][n-1]);
+        }
+
+    }
+
+    static void solve() {
+        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.d, o2.d));
+        q.add(new Node(0, 0, 0));
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            int x = cur.x;
+            int y = cur.y;
+            int d = cur.d;
+            for(int i=0;i<4;i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx>=0 && nx<n && ny>=0 && ny<n) {
+                    if(sum[nx][ny]>map[nx][ny]+sum[x][y]) {
+                        sum[nx][ny] = map[nx][ny]+sum[x][y];
+                        q.add(new Node(nx, ny, d+1));
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+class Node{
+    int x;
+    int y;
+    int d;
+    Node(int x, int y, int d) {
+        this.x = x;
+        this.y = y;
+        this.d = d;
+    }
+}
