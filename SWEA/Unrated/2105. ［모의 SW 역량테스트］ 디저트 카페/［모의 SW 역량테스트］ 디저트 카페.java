384a34b34b0e6e434e278088d865a0d289dc617f
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-05-25 21:31:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 239 ms, Memory: 63,300 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -3,10 +3,9 @@ import java.util.*;
 
 class Solution {
 
-    static int n, map[][], max=-1, v[][], ans=-1;
-    static int sx, sy;
-    static int dx[] = {-1, -1, 1, 1};
-    static int dy[] = {-1, 1, 1, -1};
+    static int n, map[][], ans, v[][], sx, sy;
+    static int dx[] = {1, 1, -1, -1};
+    static int dy[] = {1, -1, -1, 1};
 
     public static void main(String[] args) throws Exception {
 
@@ -16,13 +15,11 @@ class Solution {
         int T = Integer.parseInt(br.readLine());
 
         for(int tc=1;tc<=T;tc++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
+
+            n = Integer.parseInt(br.readLine());
 
             map = new int[n][n];
             v = new int[n][n];
-            max=-1;
-            ans=-1;
             for(int i=0;i<n;i++) {
                 st = new StringTokenizer(br.readLine());
                 for(int j=0;j<n;j++) {
@@ -30,49 +27,63 @@ class Solution {
                 }
             }
 
-            // 0, 0
-            // 0, n-1
-            // n-1, 0
-            // n-1, n-1
-            for(int i=0;i<n;i++) {
+            ans = Integer.MIN_VALUE;
+            for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++) {
                     if((i==0 && j==0) || (i==0 && j==n-1) || (i==n-1 && j==0) || (i==n-1 && j==n-1)) continue;
-                    // 시작 지점
+                    Set<Integer> set = new HashSet<>();
+                    set.add(map[i][j]);
+                    v[i][j]=1;
                     sx = i;
                     sy = j;
-                    solve(i, j, 0, 1, new HashSet<>());
+                    solve(i, j, 0, set);
                 }
+            }
+
+            if(ans==Integer.MIN_VALUE) {
+                ans = -1;
             }
 
             System.out.println("#" + tc + " " + ans);
         }
 
+        br.close();
+
     }
 
-    static void solve(int x, int y, int dir, int depth, Set<Integer> set) {
-        set.add(map[x][y]);
-        v[x][y]=1;
-
-        for(int i=0;i<4;i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            // 범위 안에 있을 때
-            if(nx>=0 && nx<n && ny>=0 && ny<n) {
-                if(depth==1 || dir==i || (dir+1)%4==i) {
-                    if(nx==sx && ny==sy) {
-                        ans = Math.max(ans, depth);
-                    }
-                    if(!set.contains(map[nx][ny]) && v[nx][ny]==0) {
-                        set.add(map[nx][ny]);
-                        v[nx][ny]=1;
-                        solve(nx, ny, i, depth+1, set);
-                        set.remove(map[nx][ny]);
-                        v[nx][ny]=0;
-                    }
-                }
-            }
+    // 왔던 길 다시 돌아 가는건 visit 로 체크
+    // 중복 디저트 냠냠 set 으로 체크
+    static void solve(int x, int y, int dir, Set<Integer> set) {
+        if(dir>=4) {
+            // 디저트 수
+//            ans = Math.max(ans, set.size());
+            return;
         }
 
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if(nx==sx && ny==sy) {
+            ans = Math.max(ans, set.size());
+            return;
+        }
+        if(nx>=0 && nx<n && ny>=0 && ny<n && v[nx][ny]==0) {
+            if(!set.contains(map[nx][ny])) {
+                // 디저트 포함 되어 있지 않으면 추가
+                set.add(map[nx][ny]);
+                // 왔던 길 체크
+                v[nx][ny]=1;
+                // 2 방향 으로 갈 수 있음
+                // 현재 방향
+                solve(nx, ny, dir, set);
+                // 다음 방향
+                solve(nx, ny, dir+1, set);
+                // back tracking
+                v[nx][ny]=0;
+                // 다시 뱉어
+                set.remove(map[nx][ny]);
+            }
+        }
     }
 
 }
+
