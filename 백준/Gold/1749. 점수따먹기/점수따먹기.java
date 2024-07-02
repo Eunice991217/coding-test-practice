73f934fa50e2f5fce29d323fdf72049d95baa88c
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-07-03 00:09:01 +0900

    [Gold IV] Title: 점수따먹기, Time: 1380 ms, Memory: 21232 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m; // 행렬
+    static int[][] map, sum;
+    static int maxN;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n+1][m+1];
+        sum = new int[n+1][m+1];
+        maxN = Integer.MIN_VALUE;
+
+        for(int i=1;i<=n;i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=m;j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + map[i][j];
+            }
+        }
+
+        for(int x=1;x<=n;x++) {
+            for(int xe=x; xe<=n;xe++) {
+                for(int y=1;y<=m;y++) {
+                    for(int ye=y;ye<=m;ye++) {
+                        maxN = Math.max(maxN, sum[xe][ye] - sum[x-1][ye] - sum[xe][y-1] + sum[x-1][y-1]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxN);
+    }
+}
