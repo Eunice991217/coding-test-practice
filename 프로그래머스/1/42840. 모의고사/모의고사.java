04124f716da0540e3b5fe7beda46bb577df714bd
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-10-23 22:18:56 +0900

    [level 1] Title: 모의고사, Time: 0.87 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,33 +4,33 @@ import java.io.*;
 class Solution {
     public int[] solution(int[] answers) {
     
+        // 가장 높은 점수를 받은 사람 return 
+        // 여럿이면 return 하는 값 오름차순 정렬 
         int[] s1 = {1, 2, 3, 4, 5};
         int[] s2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] score = new int[3];
         
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == s1[i%5]) score[0]++;
-            if(answers[i] == s2[i%8]) score[1]++;
-            if(answers[i] == s3[i%10]) score[2]++;
+        int s1N=0, s2N=0, s3N=0;
+        
+        for(int i=0;i<answers.length;i++) {
+            if(s1[i%5]==answers[i]) s1N++;
+            if(s2[i%8]==answers[i]) s2N++;
+            if(s3[i%10]==answers[i]) s3N++;
         }
         
-        List<Integer> list = new ArrayList<Integer>();
+        // 최대 점수 구하기 
+        int max = Math.max(s1N, Math.max(s2N, s3N));
         
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        List<Integer> list = new ArrayList<>();
+        if(max==s1N) list.add(1);
+        if(max==s2N) list.add(2);
+        if(max==s3N) list.add(3);
         
-        for(int i=0; i< score.length; i++) {
-            if(max == score[i]) list.add(i+1);
-        }
-        
-        int answer[] = new int[list.size()];
+        int[] ans = new int[list.size()];
         for(int i=0;i<list.size();i++) {
-            answer[i] = list.get(i);
+            ans[i] = list.get(i);
         }
         
-        // 오름차순 정렬 
-        Arrays.sort(answer); 
-        
-        return answer;
+        return ans;
     }
 }
