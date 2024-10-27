c433b9bf8f994631eb351899e96f8547f6fc0aa1
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2024-10-27 12:44:17 +0900

    [level 2] Title: 소수 찾기, Time: 18.34 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,19 +3,20 @@ import java.io.*;
 
 class Solution {
     
-    static int size, answer;
+    static int answer, size;
     static char[] arr;
     static boolean[] v;
-    static HashSet<Integer> set = new HashSet<>();
+    static HashSet<Integer> set;
     
     public int solution(String numbers) {
         answer = 0;
         size = numbers.length();
         arr = new char[size];
         v = new boolean[size];
+        set = new HashSet<>();
         
         for(int i=0;i<size;i++) {
-            arr[i] = numbers.charAt(i); // arr : 0, 1, 1 
+            arr[i] = numbers.charAt(i);
         }
         
         go(0, "");
@@ -24,15 +25,18 @@ class Solution {
         return answer;
     }
     
+    // 만들 수 있는 수 구하기 
     static void go(int idx, String str) {
-        int n;
         if(str!="") {
-            n = Integer.parseInt(str);
+            int n = Integer.parseInt(str);
             if(check(n)) {
-                set.add(n); // 소수면 set 에 넣어줌 
+                System.out.println(n);
+                set.add(n);
             }
         }
-        if(idx==size) return;
+        if(idx==size) {    
+            return;
+        }
         
         for(int i=0;i<size;i++) {
             if(v[i]) continue;
@@ -42,16 +46,16 @@ class Solution {
         }
     }
     
+    // 소수 판별 
     static boolean check(int num) {
-         if(num<=1)
-            return false;
+        if(num<=1) return false;
         
-        for(int i=2; i<=(int)Math.sqrt(num); i++){
-            if(num%i==0){
-                return false;
-            }
+        for(int i=2;i<=Math.sqrt(num);i++) {
+            if(num%i==0) return false;
         }
         return true;
+        
     }
+    
     
 }
