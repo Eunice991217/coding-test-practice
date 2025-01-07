83e5f402b7fb3465b5f6f14f089f61e137a1db67
MinKyung Kim <101406317+Eunice991217@users.noreply.github.com>
2025-01-07 20:51:42 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14624 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -15,32 +15,29 @@ class Main {
 
         arr = new int[N][3];
 
+        int rank = 1;
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             int index = Integer.parseInt(st.nextToken());
             int gold = Integer.parseInt(st.nextToken());
             int silver = Integer.parseInt(st.nextToken());
             int dong = Integer.parseInt(st.nextToken());
-            arr[i][0] = index;
-            arr[i][1] = gold * 1000003 + silver * 1000002 + dong * 1000001;
+            arr[index-1][0] = gold;
+            arr[index-1][1] = silver;
+            arr[index-1][2] = dong;
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1]-o2[1]; // 두번째 숫자 기준 오름 차순
-            }
-        });
-
-        arr[0][2] = 1;
-        for(int i=1;i<N;i++) {
-            arr[i][2] = i+1;
-            if(arr[i-1][1]==arr[i][1]) {
-                arr[i][2] = arr[i-1][2];
+        for(int i=0;i<N;i++) {
+            if(arr[i][0] > arr[K-1][0]) {
+                rank++;
+            } else if(arr[i][0] == arr[K-1][0] && arr[i][1] > arr[K-1][1]) {
+                rank++;
+            } else if(arr[i][0] == arr[K-1][0] && arr[i][1] == arr[K-1][1] && arr[i][2] > arr[K-1][2]) {
+                rank++;
             }
         }
 
-        System.out.println(arr[K-1][2]);
+        System.out.println(rank);
 
     }
 
